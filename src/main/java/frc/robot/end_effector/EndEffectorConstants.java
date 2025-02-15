74a2070129927ccb053ead35697efba974f5a8f4
StarbuckBarista <carlos.salazar@barstowschool.org>
2025-02-15 14:14:29 -0600

    Fixed Reduction Typo

--- src/main/java/frc/robot/end_effector/EndEffectorConstants.java
+++ src/main/java/frc/robot/end_effector/EndEffectorConstants.java
@@ -3,13 +3,13 @@ package frc.robot.end_effector;
 public class EndEffectorConstants {
     
     public static final int coralIntakeCAN = 38; //TODO coral Intake CAN ID AFTER ROBOT IS WIRED
-    public static final double coralIntakeGearReducation = (36/56);
+    public static final double coralIntakeGearReduction = (36/56);
     public static final int coralIntakecurrentLimit = 40;
     public static final int algaeIntakeCAN = 39; //TODO algae Intake CAN ID AFTER ROBOT IS WIRED
-    public static final double algaeIntakeGearReducation = (36/56);
+    public static final double algaeIntakeGearReduction = (36/56);
     public static final int algaeIntakecurrentLimit = 40; 
     public static final int algaeWristCAN = 39; //TODO algae Wrist CAN ID AFTER ROBOT IS WIRED
-    public static final double algaeWristGearReducation = (42/42);
+    public static final double algaeWristGearReduction = (42/42);
     public static final int algaeWristcurrentLimit = 40; 
     public static final int coralIntakeBeamBreakCAN = 40; //TODO Beam Break CAN ID AFTER ROBOT IS WIRED
     public static final int AlgaeIntakeLaserCAN = 40; //TODO Laser CAN CAN ID AFTER ROBOT IS WIRED
