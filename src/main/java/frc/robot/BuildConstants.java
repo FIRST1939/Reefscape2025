bcb754d9a0ae346ef019c213cf2ca7ccf29ce8a7
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2025-02-22 11:40:54 -0600

    created Commands

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "";
-  public static final String VERSION = "";
-  public static final int GIT_REVISION = 0;
-  public static final String GIT_SHA = "";
-  public static final String GIT_DATE = "";
-  public static final String GIT_BRANCH = "";
-  public static final String BUILD_DATE = "";
-  public static final long BUILD_UNIX_TIME = 0;
-  public static final int DIRTY = 0;
+  public static final String MAVEN_NAME = "Reefscape2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 62;
+  public static final String GIT_SHA = "dba211dd4864eea5f611dc2b962700c914ca4834";
+  public static final String GIT_DATE = "2025-02-22 09:32:41 CST";
+  public static final String GIT_BRANCH = "feature/commands";
+  public static final String BUILD_DATE = "2025-02-22 11:40:28 CST";
+  public static final long BUILD_UNIX_TIME = 1740246028684L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
