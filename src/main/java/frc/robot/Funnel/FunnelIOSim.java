1759c43df60a54b8c910faf097ec965fcdfb4bde
Sakana <69824405+wljf817@users.noreply.github.com>
2025-02-22 14:32:55 -0600

    Funnel IO Sim

--- src/main/java/frc/robot/Funnel/FunnelIOSim.java
+++ src/main/java/frc/robot/Funnel/FunnelIOSim.java
@@ -1,5 +1,43 @@
 package frc.robot.Funnel;
 
-public class FunnelIOSim {
-    
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+// Funnel Simulation
+public class FunnelIOSim implements FunnelIO {
+ 
+    private final DCMotorSim FunnelMotorLeader = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(DCMotor.getNeoVortex(1), 0.001, 1.0),
+        DCMotor.getNeoVortex(1)
+    );
+
+    private final DCMotorSim FunnelMotorFollower = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(DCMotor.getNeoVortex(1), 0.001, 1.0),
+        DCMotor.getNeoVortex(1)
+    );
+
+    private double appliedVolts = 0.0;
+
+    @Override
+    public void updateInputs(FunnelIOInputs inputs) {
+
+        FunnelMotorLeader.setInputVoltage(appliedVolts);
+        FunnelMotorLeader.update(0.02);
+        
+        FunnelMotorFollower.setInputVoltage(-appliedVolts);
+        FunnelMotorFollower.update(0.02);
+
+        inputs.funnelPosition = 0.0;
+        inputs.funnelVelocity = 0.0;
+        inputs.funnelVoltage = 0.0;
+        inputs.funnelCurrent = 0.0;
+        inputs.funnelBeambreak = false;
+    }
+
+    @Override
+    public void setFunnelVoltage(double volts) {
+        
+        appliedVolts = volts;
+    }
 }
