f55f192b782a657aa85f2d3cab85497f1373a6d3
TRIKAINANWU <keiran.vu.tri@gmail.com>
2025-02-24 17:18:28 -0600

    first attempt at algae outtake command

--- src/main/java/frc/robot/commands/AlgaeOuttake.java
+++ src/main/java/frc/robot/commands/AlgaeOuttake.java
@@ -1,12 +1,26 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
-
+import frc.robot.end_effector.EndEffector;
 public class AlgaeOuttake extends Command{
+    private EndEffector endEffector;
+    private DoubleSupplier algaeouttakespeedSupplier;
+    public AlgaeOuttake(EndEffector endEffector, DoubleSupplier algaeouttakespeed) {
+        this.endEffector=endEffector;
+        this.addRequirements(endEffector);
+    }
 
-    public AlgaeOuttake(double algaeouttakespeed) {
+public AlgaeOuttake(double algaeouttakespeed) {
         //TODO Auto-generated constructor stub
     }
+
+@Override
+public void initialize () 
+{
+    endEffector.runVoltage(0,algaeouttakespeedSupplier.getAsDouble(),0);
+}
 
     
 }
