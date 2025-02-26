e579df77c77a3f43192b098c461afdf326e98a92
AmeliaK550 <akalahurka10@gmail.com>
2025-02-25 18:51:04 -0600

    fixed import statement

--- src/main/java/frc/robot/commands/FunnelIntake.java
+++ src/main/java/frc/robot/commands/FunnelIntake.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 
 
-
+import frc.
 import edu.wpi.first.wpilibj2.command.Command;
 
 
--- src/main/java/frc/robot/commands/WristPivot.java
+++ src/main/java/frc/robot/commands/WristPivot.java
@@ -3,7 +3,7 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.end_effector.EndEffector;
+import frc.robot.subsystems.end_effector.EndEffector;
 
 public class WristPivot extends Command{
     private double wristspeed;
