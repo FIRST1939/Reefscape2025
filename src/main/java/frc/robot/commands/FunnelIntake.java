b059766b51defa6ebf656fa0c3f1cef9dd8a8c95
AmeliaK550 <akalahurka10@gmail.com>
2025-02-25 18:57:54 -0600

    funnel command added

--- src/main/java/frc/robot/commands/FunnelIntake.java
+++ src/main/java/frc/robot/commands/FunnelIntake.java
@@ -1,10 +1,25 @@
 package frc.robot.commands;
 
 
-import frc.
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.funnel.Funnel;
+import frc.robot.subsystems.end_effector.EndEffector;
 
 
 public class FunnelIntake extends Command{
+    private double funnelspeed;
+    private Funnel funnel;
+      public FunnelIntake(EndEffector endEffector, double funnelspeed) {
+        this.funnelspeed = funnelspeed;
+        
+    }
+
+@Override
+public void initialize () 
+{
+   funnel.runVoltage(funnelspeed);
+
     
+}
 }
