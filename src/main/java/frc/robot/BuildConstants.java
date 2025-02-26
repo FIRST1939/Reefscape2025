bf49303ee54d52929e5c2ada0b222841060fb501
AmeliaK550 <akalahurka10@gmail.com>
2025-02-25 18:14:58 -0600

    wrist pivot command

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Reefscape2025-1";
+  public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 66;
-  public static final String GIT_SHA = "a9fbacdecb906fa4f59a34bbe85f7d5c50a6baf8";
-  public static final String GIT_DATE = "2025-02-22 14:32:27 CST";
+  public static final int GIT_REVISION = 71;
+  public static final String GIT_SHA = "03d82c745adac1b888cf99f7c89498c48147c7b8";
+  public static final String GIT_DATE = "2025-02-24 17:18:37 CST";
   public static final String GIT_BRANCH = "feature/commands";
-  public static final String BUILD_DATE = "2025-02-22 15:14:38 CST";
-  public static final long BUILD_UNIX_TIME = 1740258878113L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-25 17:35:09 CST";
+  public static final long BUILD_UNIX_TIME = 1740526509140L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
--- src/main/java/frc/robot/commands/WristPivot.java
+++ src/main/java/frc/robot/commands/WristPivot.java
@@ -1,7 +1,29 @@
 package frc.robot.commands;
 
+
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.end_effector.EndEffector;
 
 public class WristPivot extends Command{
+    private double wristspeed;
+    private EndEffector endEffector;
+    public WristPivot(EndEffector endEffector, double wristspeed) {
+        this.wristspeed = wristspeed;
+        
+    }
+
+@Override
+public void initialize () 
+{
+    endEffector.runVoltage(0,0,wristspeed);
+}
+
     
 }
+    
+
+
+    
+
+
