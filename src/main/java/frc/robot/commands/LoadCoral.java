e9f9bf51ec894e97d6122295f6f6f8f5194afc67
StarbuckBarista <carlitos.salazar@protonmail.com>
2025-03-06 01:22:43 -0600

    LoadCoral Deadline

--- src/main/java/frc/robot/commands/LoadCoral.java
+++ src/main/java/frc/robot/commands/LoadCoral.java
@@ -3,19 +3,21 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.SetPointConstants;
 import frc.robot.subsystems.end_effector.EndEffector;
 import frc.robot.subsystems.funnel.Funnel;
 
-public class LoadCoral extends ParallelCommandGroup{
+public class LoadCoral extends ParallelDeadlineGroup{
 
     public LoadCoral (Funnel funnel, EndEffector endEffector, double coralInSpeed, double coralBackSpeed) {
-        this.addCommands(
-            new AutomaticFunnel(funnel, SetPointConstants.FUNNEL_INTAKE_SPEED, SetPointConstants.FUNNEL_STUCK_SPEED),
+
+        super(
             Commands.sequence(
                 new AutomaticEndEffectorIn(endEffector, coralInSpeed),
                 new AutomaticEndEffectorBack(endEffector, coralBackSpeed)
-            )
+            ),
+            new AutomaticFunnel(funnel, SetPointConstants.FUNNEL_INTAKE_SPEED, SetPointConstants.FUNNEL_STUCK_SPEED)
         );
     }
 }
