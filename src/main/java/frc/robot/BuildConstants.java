d833f28d78da0547f3304ffb17424072ba85a9c2
StarbuckBarista <carlitos.salazar@protonmail.com>
2025-03-06 23:18:33 -0600

    Added Reef Coral Positions for 2d Translations -Maaz Hawa

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 172;
-  public static final String GIT_SHA = "dd1c38739a2651022954ba9cfd43133ccabda948";
-  public static final String GIT_DATE = "2025-03-06 22:00:58 CST";
+  public static final int GIT_REVISION = 173;
+  public static final String GIT_SHA = "e2043a9c4cdba3779b4b4d81db60443ee52c55c3";
+  public static final String GIT_DATE = "2025-03-06 22:28:55 CST";
   public static final String GIT_BRANCH = "cmo";
-  public static final String BUILD_DATE = "2025-03-06 22:27:36 CST";
-  public static final long BUILD_UNIX_TIME = 1741321656634L;
+  public static final String BUILD_DATE = "2025-03-06 23:17:55 CST";
+  public static final long BUILD_UNIX_TIME = 1741324675126L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/SetPointConstants.java
+++ src/main/java/frc/robot/SetPointConstants.java
@@ -17,5 +17,30 @@ public class SetPointConstants {
     public static final double FUNNEL_STUCK_SPEED = -1.65;
     public static final double CORAL_OUTTAKE_SPEED = 20.0;
 
-    public static final Translation2d[] REEF_CORAL_POSITIONS = {};
+    public static final Translation2d[] REEF_CORAL_POSITIONS = {
+        new Translation2d(3.21578679657, 4.18578679657),
+        new Translation2d(3.21578679657, 3.85678679657),
+        new Translation2d(3.84478679657, 2.92178679657),
+        new Translation2d(3.98678679657, 2.83978679657),
+        new Translation2d(4.95878679657, 2.83978679657),
+        new Translation2d(5.45678679657, 2.63578679657),
+        new Translation2d(5.77249922344, 3.85678679657),
+        new Translation2d(5.32583679657, 4.02078679657),
+        new Translation2d(5.24378679657, 5.01778679657),
+        new Translation2d(4.95878679657, 5.20278679657),
+        new Translation2d(3.98678679657, 5.20278679657),
+        new Translation2d(3.70178679657, 5.03878679657),
+        new Translation2d(14.321, 3.868),
+        new Translation2d(14.321,  4.197),
+        new Translation2d(13.835, 5.039),
+        new Translation2d(13.550, 5.203),
+        new Translation2d(12.578, 5.203),
+        new Translation2d(12.293, 5.039),
+        new Translation2d(11.807, 4.197),
+        new Translation2d(11.807, 3.868),
+        new Translation2d(12.293, 3.026),
+        new Translation2d(12.578, 2.861),
+        new Translation2d(13.550, 2.861),
+        new Translation2d(13.835, 3.026),
+    };
 }
