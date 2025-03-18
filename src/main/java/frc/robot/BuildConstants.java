b631be0913e55f93ec9c2362f7743b5ae936127c
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2025-03-18 11:22:56 -0500

    Auton Changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 188;
-  public static final String GIT_SHA = "95c225d655e240f21306f6de90149a46205875ea";
-  public static final String GIT_DATE = "2025-03-08 00:07:30 CST";
+  public static final int GIT_REVISION = 189;
+  public static final String GIT_SHA = "5b27c4079a5c5e2be847b1b6f1674afd6ebff3a8";
+  public static final String GIT_DATE = "2025-03-13 16:49:35 CDT";
   public static final String GIT_BRANCH = "cmo";
-  public static final String BUILD_DATE = "2025-03-08 15:35:57 CST";
-  public static final long BUILD_UNIX_TIME = 1741469757155L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-14 12:12:51 CDT";
+  public static final long BUILD_UNIX_TIME = 1741972371999L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
