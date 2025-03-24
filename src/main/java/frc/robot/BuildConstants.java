255334da43ab885e1cf7182a371ed2eb0b16e948
AmeliaK550 <akalahurka10@gmail.com>
2025-03-24 15:10:59 -0500

    parameter made

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 191;
-  public static final String GIT_SHA = "91117a519f24248efecf6394781e36269cadd2ee";
-  public static final String GIT_DATE = "2025-03-19 13:39:03 CDT";
-  public static final String GIT_BRANCH = "fix/organization";
-  public static final String BUILD_DATE = "2025-03-21 01:34:54 CDT";
-  public static final long BUILD_UNIX_TIME = 1742538894171L;
+  public static final int GIT_REVISION = 199;
+  public static final String GIT_SHA = "789dca90eb74c381ba23d5540f674afa8bda3b96";
+  public static final String GIT_DATE = "2025-03-24 13:57:31 CDT";
+  public static final String GIT_BRANCH = "feature/leds";
+  public static final String BUILD_DATE = "2025-03-24 15:08:21 CDT";
+  public static final long BUILD_UNIX_TIME = 1742846901661L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
-import java.util.Map;
+import java.util.function.DoubleSupplier;
 
 public class LEDs extends SubsystemBase {
     
@@ -25,60 +25,43 @@ public class LEDs extends SubsystemBase {
     }
 
     @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-       // LEDPattern pattern = LEDPattern.progressMaskLayer(() -> m_elevator.getHeight() / m_elevator.getMaxHeight());
-       // pattern.applyTo(ledBuffer);
-
-        // Set the LEDs
+    public void periodic () {
+        
+        pattern.applyTo(ledBuffer);
         ledStrip.setData(ledBuffer);
     }
 
-    public void setSolidPatternC1() {
-        LEDPattern green = LEDPattern.solid(Color.kGreen);
-        green.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
+    public void setSolidPatternCoralReadyToScore() {
+
+        pattern = LEDPattern.solid(Color.kGreen);
     }
 
-    public void setSolidPatternC2() {
-        LEDPattern purple = LEDPattern.solid(Color.kPurple);
-        purple.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
+    public void setSolidPatternReefOuttaking() {
+       
+        pattern = LEDPattern.solid(Color.kPurple);
+     
     }
 
-    public void setSolidPatternA1() {
-        LEDPattern teal = LEDPattern.solid(Color.kTeal);
-        teal.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
+    public void setSolidPatternAlgaeReadyToScore() {
+        
+        pattern = LEDPattern.solid(Color.kTeal);
+    
     }
 
-    public void setSolidPatternA2() {
-        LEDPattern purple = LEDPattern.solid(Color.kPurple);
-        purple.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
+    public void setSolidPatternAlgaeReefOuttaking() {
+     
+        pattern = LEDPattern.solid(Color.kPurple);
+      
     }
 
     public void setRainbowPattern() {
-        LEDPattern rainbow = LEDPattern.rainbow(255, 128); // Example
-        rainbow.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
+
+        pattern = LEDPattern.rainbow(255, 128);
     }
 
-    public void setYellowProgressMask() {
-        LEDPattern pattern = LEDPattern.progressMaskLayer(() -> m_elevator.getHeight() / m_elevator.getMaxHeight());
-        pattern.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
-    }
+    public void setElevatorProgress (DoubleSupplier elevatorHeight, double targetHeight) {
 
-    public void setRedProgressMask() {
-        LEDPattern pattern = LEDPattern.progressMaskLayer(() -> m_elevator.getHeight() / m_elevator.getMaxHeight());
-        pattern.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
-    }
-
-    public void setBlueAndGreenFlamesPattern() {
-        LEDPattern steps = LEDPattern.steps(Map.of(0, Color.kGreen, 0.5, Color.kBlue));
-        steps.applyTo(ledBuffer);
-        ledStrip.setData(ledBuffer);
+        LEDPattern base = LEDPattern.solid(Color.kPink);
+        pattern = base.mask(LEDPattern.progressMaskLayer(() -> elevatorHeight.getAsDouble() / targetHeight));
     }
 }
