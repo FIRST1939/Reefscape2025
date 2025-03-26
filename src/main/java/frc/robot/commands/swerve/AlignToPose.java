bb04432fb356c8b0a2ca297e24f02f7d0e07ccdb
StarbuckBarista <carlitos.salazar@protonmail.com>
2025-03-26 18:49:43 -0500

    Pathplanning Generation

--- src/main/java/frc/robot/commands/swerve/AlignToPose.java
+++ src/main/java/frc/robot/commands/swerve/AlignToPose.java
@@ -1,11 +1,15 @@
 package frc.robot.commands.swerve;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swerve.Swerve;
 
@@ -13,89 +17,59 @@ public class AlignToPose extends Command {
     
     private final Swerve swerve;
     private final Supplier<Pose2d> poseSupplier;
-    private Pose2d pose;
 
-    private final TrapezoidProfile xController = new TrapezoidProfile(
-        new TrapezoidProfile.Constraints(
-            1.0, 
-            1.0
-        )
-    );
-
-    private final TrapezoidProfile yController = new TrapezoidProfile(
-        new TrapezoidProfile.Constraints(
-            1.0, 
-            1.0
-        )
-    );
-
-    private final TrapezoidProfile thetaController = new TrapezoidProfile(
-        new TrapezoidProfile.Constraints(
-            1.0, 
-            1.0
-        )
-    );
-
-    private final Timer timer = new Timer();
-    private double lastTimestamp;
+    private Command pathfindingCommand;
 
     public AlignToPose (Swerve swerve, Supplier<Pose2d> poseSupplier) {
 
         this.swerve = swerve;
         this.poseSupplier = poseSupplier;
-
-        this.addRequirements(this.swerve);
     }
 
     @Override
     public void initialize () {
 
-        this.pose = this.poseSupplier.get();
-
-        this.timer.restart();
-        this.lastTimestamp = 0.0;
-    }
-
-    @Override
-    public void execute () {
-
-        double dt = this.timer.get() - this.lastTimestamp;
-        this.lastTimestamp = this.timer.get();
-
         Pose2d currentPose = this.swerve.getPose();
-        ChassisSpeeds currentVelocity = this.swerve.getFieldVelocity();
+        Pose2d targetPose = this.poseSupplier.get();
 
-        double vx = this.xController.calculate(
-            dt, 
-            new TrapezoidProfile.State(currentPose.getX(), currentVelocity.vxMetersPerSecond),
-            new TrapezoidProfile.State(this.pose.getX(), 0.0)
-        ).velocity;
+        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
+            new Pose2d(currentPose.getTranslation(), targetPose.minus(currentPose).getTranslation().getAngle()),
+            targetPose
+        );
 
-        double vy = this.yController.calculate(
-            dt, 
-            new TrapezoidProfile.State(currentPose.getY(), currentVelocity.vyMetersPerSecond),
-            new TrapezoidProfile.State(this.pose.getY(), 0.0)
-        ).velocity;
+        PathConstraints constraints = new PathConstraints(
+            4.0,
+            3.0,
+            Math.PI * 2,
+            Math.PI * 2,
+            12.0
+        );
 
-        double currentHeading = currentPose.getRotation().getRadians();
-        double goalHeading = this.pose.getRotation().getRadians();
+        PathPlannerPath path = new PathPlannerPath(
+            waypoints,
+            constraints,
+            null,
+            new GoalEndState(0.0, targetPose.getRotation())
+        );
 
-        double deltaHeading = Math.atan2(Math.sin(goalHeading - currentHeading), Math.cos(goalHeading - currentHeading));
-        double wrappedGoalHeading = currentHeading + deltaHeading;
+        path.preventFlipping = true;
 
-        double omega = this.thetaController.calculate(
-            dt, 
-            new TrapezoidProfile.State(currentHeading, currentVelocity.omegaRadiansPerSecond),
-            new TrapezoidProfile.State(wrappedGoalHeading, 0.0)
-        ).velocity;
-
-        this.swerve.driveFieldOriented(ChassisSpeeds.fromFieldRelativeSpeeds(vx, vy, omega, currentPose.getRotation()));
+        this.pathfindingCommand = AutoBuilder.followPath(path);
+        this.pathfindingCommand.schedule();
     }
 
     @Override
     public boolean isFinished () {
 
-        double elapsedTime = this.timer.get();
-        return this.xController.isFinished(elapsedTime) && this.yController.isFinished(elapsedTime) && this.thetaController.isFinished(elapsedTime);
+        return this.pathfindingCommand.isFinished();
+    }
+
+    @Override
+    public void end (boolean interrupted) {
+
+        if (this.pathfindingCommand != null) {
+
+            this.pathfindingCommand.cancel();
+        }
     }
 }
--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 214;
-  public static final String GIT_SHA = "1e6f1a0eb174b9d0177a735bcb3b19e3dfab712d";
-  public static final String GIT_DATE = "2025-03-26 13:45:29 CDT";
+  public static final int GIT_REVISION = 215;
+  public static final String GIT_SHA = "fa551bfc9815798c34111466e51f8fc66d3ddc94";
+  public static final String GIT_DATE = "2025-03-26 18:17:01 CDT";
   public static final String GIT_BRANCH = "feature/reef_alignment";
-  public static final String BUILD_DATE = "2025-03-26 15:40:26 CDT";
-  public static final long BUILD_UNIX_TIME = 1743021626344L;
+  public static final String BUILD_DATE = "2025-03-26 18:49:22 CDT";
+  public static final long BUILD_UNIX_TIME = 1743032962078L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
