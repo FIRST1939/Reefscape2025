86873babf48d53821f08db0ec49b22b1eabee734
StarbuckBarista <carlitos.salazar@protonmail.com>
2025-03-26 19:22:46 -0500

    Target Transforms

--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 215;
-  public static final String GIT_SHA = "fa551bfc9815798c34111466e51f8fc66d3ddc94";
-  public static final String GIT_DATE = "2025-03-26 18:17:01 CDT";
+  public static final int GIT_REVISION = 217;
+  public static final String GIT_SHA = "86e0f02b1759d0d81d18b5c95c97c26d57e76b94";
+  public static final String GIT_DATE = "2025-03-26 19:03:26 CDT";
   public static final String GIT_BRANCH = "feature/reef_alignment";
-  public static final String BUILD_DATE = "2025-03-26 18:49:22 CDT";
-  public static final long BUILD_UNIX_TIME = 1743032962078L;
+  public static final String BUILD_DATE = "2025-03-26 19:21:07 CDT";
+  public static final long BUILD_UNIX_TIME = 1743034867383L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/util/RobotGoals.java
+++ src/main/java/frc/robot/util/RobotGoals.java
@@ -58,6 +58,118 @@ public class RobotGoals {
         targetAlgaePose = closestAlgaePose;
     }
 
+    public static void transformTargetCW () {
+
+        Pose2d[] reefCoralPoses;
+        Pose2d[] reefAlgaePoses;
+
+        var alliance = DriverStation.getAlliance();
+
+        if (alliance.isPresent() && alliance.get() == Alliance.Red) {
+
+            reefCoralPoses = SetPointConstants.RED_REEF_CORAL_POSES;
+            reefAlgaePoses = SetPointConstants.RED_REEF_ALGAE_POSES;
+        } else {
+
+            reefCoralPoses = SetPointConstants.BLUE_REEF_CORAL_POSES;
+            reefAlgaePoses = SetPointConstants.BLUE_REEF_ALGAE_POSES;
+        }
+
+        int coralTargetIndex = 0;
+        
+        for (int i = 0; i < reefCoralPoses.length; i++) {
+
+            if (reefCoralPoses[i].equals(targetCoralPose)) {
+
+                coralTargetIndex = i;
+                break;
+            }
+        }
+
+        int algaeTargetIndex = 0;
+
+        for (int i = 0; i < reefAlgaePoses.length; i++) {
+
+            if (reefAlgaePoses[i].equals(targetAlgaePose)) {
+
+                algaeTargetIndex = i;
+                break;
+            }
+        }
+
+        coralTargetIndex++;
+        algaeTargetIndex++;
+
+        if (coralTargetIndex >= reefCoralPoses.length) {
+
+            coralTargetIndex = 0;
+        }
+
+        if (algaeTargetIndex >= reefAlgaePoses.length) {
+
+            algaeTargetIndex = 0;
+        }
+
+        targetCoralPose = reefCoralPoses[coralTargetIndex];
+        targetAlgaePose = reefAlgaePoses[algaeTargetIndex];
+    }
+
+    public static void transformTargetCCW () {
+
+        Pose2d[] reefCoralPoses;
+        Pose2d[] reefAlgaePoses;
+
+        var alliance = DriverStation.getAlliance();
+
+        if (alliance.isPresent() && alliance.get() == Alliance.Red) {
+
+            reefCoralPoses = SetPointConstants.RED_REEF_CORAL_POSES;
+            reefAlgaePoses = SetPointConstants.RED_REEF_ALGAE_POSES;
+        } else {
+
+            reefCoralPoses = SetPointConstants.BLUE_REEF_CORAL_POSES;
+            reefAlgaePoses = SetPointConstants.BLUE_REEF_ALGAE_POSES;
+        }
+
+        int coralTargetIndex = 0;
+        
+        for (int i = 0; i < reefCoralPoses.length; i++) {
+
+            if (reefCoralPoses[i].equals(targetCoralPose)) {
+
+                coralTargetIndex = i;
+                break;
+            }
+        }
+
+        int algaeTargetIndex = 0;
+
+        for (int i = 0; i < reefAlgaePoses.length; i++) {
+
+            if (reefAlgaePoses[i].equals(targetAlgaePose)) {
+
+                algaeTargetIndex = i;
+                break;
+            }
+        }
+
+        coralTargetIndex--;
+        algaeTargetIndex--;
+
+        if (coralTargetIndex < 0) {
+
+            coralTargetIndex = reefCoralPoses.length - 1;
+        }
+
+        if (algaeTargetIndex < 0) {
+
+            algaeTargetIndex = reefAlgaePoses.length - 1;
+        }
+
+        targetCoralPose = reefCoralPoses[coralTargetIndex];
+        targetAlgaePose = reefAlgaePoses[algaeTargetIndex];
+    }
+
     public static Pose2d getTargetCoralPose () {
 
         return targetCoralPose;
