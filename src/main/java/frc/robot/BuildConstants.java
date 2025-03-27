81a764076c478ead4ffdb90fe8f61e279bf1627e
1939DriverStation <barstowrobotics1939@gmail.com>
2025-03-27 16:52:43 -0500

    PP Tuning

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 195;
-  public static final String GIT_SHA = "7b5ea2054ac8c7ccc4bea666debbd580e80893b5";
-  public static final String GIT_DATE = "2025-03-21 02:11:32 CDT";
+  public static final int GIT_REVISION = 214;
+  public static final String GIT_SHA = "97d0759f508d04177604100be821591870168be1";
+  public static final String GIT_DATE = "2025-03-27 02:00:05 CDT";
   public static final String GIT_BRANCH = "feature/pathplanner";
-  public static final String BUILD_DATE = "2025-03-21 02:16:11 CDT";
-  public static final long BUILD_UNIX_TIME = 1742541371020L;
+  public static final String BUILD_DATE = "2025-03-27 16:48:01 CDT";
+  public static final long BUILD_UNIX_TIME = 1743112081917L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
