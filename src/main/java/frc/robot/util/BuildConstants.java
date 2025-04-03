4045d0f9256a3db1035423bb9cb84d0dd33fab80
1939DriverStation <barstowrobotics1939@gmail.com>
2025-04-03 09:04:53 -0500

    Builds

--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 305;
-  public static final String GIT_SHA = "d4284f9b14a51e928d314fc7130c9ab072455ab9";
-  public static final String GIT_DATE = "2025-04-02 17:55:33 CDT";
+  public static final int GIT_REVISION = 306;
+  public static final String GIT_SHA = "09aa99aa81d49e5fd7ca291144b4781e82ffd721";
+  public static final String GIT_DATE = "2025-04-02 20:03:54 CDT";
   public static final String GIT_BRANCH = "practice/ok";
-  public static final String BUILD_DATE = "2025-04-02 19:58:29 CDT";
-  public static final long BUILD_UNIX_TIME = 1743641909818L;
+  public static final String BUILD_DATE = "2025-04-03 09:04:45 CDT";
+  public static final long BUILD_UNIX_TIME = 1743689085381L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
