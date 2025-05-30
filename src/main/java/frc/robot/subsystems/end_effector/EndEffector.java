69e3767eb97ab0257594e14faa0ae3d6fbad4c2a
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2025-05-30 11:28:24 -0500

    Preliminary Feedforward/back

--- src/main/java/frc/robot/subsystems/end_effector/EndEffector.java
+++ src/main/java/frc/robot/subsystems/end_effector/EndEffector.java
@@ -2,12 +2,17 @@ package frc.robot.subsystems.end_effector;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class EndEffector extends SubsystemBase {
     
     private final EndEffectorIO io;
     private final EndEffectorIOInputsAutoLogged inputs = new EndEffectorIOInputsAutoLogged();
+
+     private final SimpleMotorFeedforward coralIntakeFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
+    private final PIDController algaeWristFeedback = new PIDController(0.0, 0.0, 0.0);
 
     public EndEffector (EndEffectorIO io) {
 
