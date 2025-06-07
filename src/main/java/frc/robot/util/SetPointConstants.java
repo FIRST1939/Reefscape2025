7c548032e7bff5a92215cea8a615949c987bf58e
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2025-06-06 23:33:01 -0500

    created SetPointConstants

---
+++ src/main/java/frc/robot/util/SetPointConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.util;
+
+public class SetPointConstants {
+
+    public static final double CORAL_INTAKE_HEIGHT = 0.0; //TODO Config lc + heights
+    public static final double CORAL_OUTTAKE_HEIGHT_L2 = 0.0; //TODO Config lc + heights
+    public static final double CORAL_OUTTAKE_HEIGHT_L3 = 0.0; //TODO Config lc + heights
+    public static final double CORAL_OUTTAKE_HEIGHT_L4 = 0.0; //TODO Config lc + heights
+
+    public static final double FUNNEL_INTAKE_VOLTAGE = 5.0;
+    
+}
