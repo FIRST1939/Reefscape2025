ba3d8d5eb35b4457c1d3348c472d306a746e9fe6
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2025-06-08 00:24:45 -0500

    IndexCoral EndEffector command

---
+++ src/main/java/frc/robot/commands/end_effector/IndexCoral.java
@@ -0,0 +1,40 @@
+package frc.robot.commands.end_effector;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.end_effector.EndEffector;
+import frc.robot.util.SetPointConstants;
+
+public class IndexCoral extends Command {
+    
+    private final EndEffector endEffector;
+    private boolean isFinished;
+
+    public IndexCoral (EndEffector endEffector) {
+
+        this.endEffector = endEffector;
+        this.addRequirements(this.endEffector);
+
+        new Trigger(this.endEffector::getCoralIntakeBeambreak).onTrue(Commands.runOnce(() -> this.isFinished = true));
+    }
+
+    @Override
+    public void initialize () {
+
+        this.isFinished = false;
+        this.endEffector.setCoralIntakeVelocity(SetPointConstants.CORAL_INDEX_SPEED);
+    }
+
+    @Override
+    public boolean isFinished () {
+
+        return this.isFinished;
+    }
+
+    @Override
+    public void end (boolean interrupted) {
+
+        this.endEffector.setCoralIntakeVelocity(0.0);
+    }
+}
