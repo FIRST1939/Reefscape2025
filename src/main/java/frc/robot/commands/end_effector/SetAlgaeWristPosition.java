2f6031dbabe765323a3521fc8afeed50e36b4187
Maaz-Hawa <156148130+Maaz-Hawa@users.noreply.github.com>
2025-07-12 18:38:58 -0500

    Created SetAlgaeWristPosition Command

---
+++ src/main/java/frc/robot/commands/end_effector/SetAlgaeWristPosition.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.end_effector;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.end_effector.EndEffector;
+import frc.robot.util.SetPointConstants;
+
+public class SetAlgaeWristPosition extends Command {
+    
+    private final EndEffector endEffector;
+    private final double algaeWristPosition;
+    
+    public SetAlgaeWristPosition(EndEffector endEffector, double algaeWristPosition) {
+        this.endEffector = endEffector;
+        this.algaeWristPosition = algaeWristPosition;
+        addRequirements(endEffector); 
+    }
+
+    @Override
+    public void initialize() {
+        endEffector.setAlgaeWristPosition(algaeWristPosition);
+        endEffector.setAlgaeIntakeVoltage(SetPointConstants.ALGAE_INTAKE_VOLTAGE);
+    }
+
+    @Override
+    public void execute() {
+        endEffector.setAlgaeWristPosition(algaeWristPosition);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        endEffector.setAlgaeWristPosition(SetPointConstants.ALGAE_HOLD_WRIST_POSITION);
+        endEffector.setAlgaeIntakeVoltage(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
