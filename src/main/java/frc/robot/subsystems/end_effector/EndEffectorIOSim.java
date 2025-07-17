000e85d543cd72b9251ce4faaa3368f814e829d7
StarbuckBarista <66986004+StarbuckBarista@users.noreply.github.com>
2025-07-16 21:27:47 -0500

    Set Position Through SparkFlexSim

--- src/main/java/frc/robot/subsystems/end_effector/EndEffectorIOSim.java
+++ src/main/java/frc/robot/subsystems/end_effector/EndEffectorIOSim.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.end_effector;
 
 import com.revrobotics.sim.SparkFlexSim;
 import com.revrobotics.sim.SparkLimitSwitchSim;
-import com.revrobotics.sim.SparkRelativeEncoderSim;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
@@ -18,10 +17,6 @@ public class EndEffectorIOSim extends EndEffectorIOVortex {
     private final SparkFlexSim coralIntakeMotor = new SparkFlexSim(super.coralIntakeMotor, DCMotor.getNeoVortex(1));
     private final SparkFlexSim algaeIntakeMotor = new SparkFlexSim(super.algaeIntakeMotor, DCMotor.getNeoVortex(1));
     private final SparkFlexSim algaeWristMotor = new SparkFlexSim(super.algaeWristMotor, DCMotor.getNeoVortex(1));
-
-    private final SparkRelativeEncoderSim coralIntakeEncoder = new SparkRelativeEncoderSim(super.coralIntakeMotor);
-    private final SparkRelativeEncoderSim algaeIntakeEncoder = new SparkRelativeEncoderSim(super.algaeIntakeMotor);
-    private final SparkRelativeEncoderSim algaeWristEncoder = new SparkRelativeEncoderSim(super.algaeWristMotor);
 
     private final SparkLimitSwitchSim coralBeambreak = this.coralIntakeMotor.getForwardLimitSwitchSim();
 
@@ -62,9 +57,9 @@ public class EndEffectorIOSim extends EndEffectorIOVortex {
 
         super();
 
-        this.coralIntakeEncoder.setPosition(super.coralIntakeEncoder.getPosition());
-        this.algaeIntakeEncoder.setPosition(super.algaeIntakeEncoder.getPosition());
-        this.algaeWristEncoder.setPosition(super.algaeWristEncoder.getPosition());
+        this.coralIntakeMotor.setPosition(super.coralIntakeEncoder.getPosition());
+        this.algaeIntakeMotor.setPosition(super.algaeIntakeEncoder.getPosition());
+        this.algaeWristMotor.setPosition(super.algaeWristEncoder.getPosition());
     }
 
     @Override
