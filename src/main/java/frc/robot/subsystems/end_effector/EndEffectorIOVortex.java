d8db58a8813c3aa4634ec5aa4b3702b2e722da16
Maaz-Hawa <hawamaaz5@gmail.com>
2025-07-21 00:28:05 -0500

    D tuned after P achieved steady state oscillation

--- src/main/java/frc/robot/subsystems/end_effector/EndEffectorIOVortex.java
+++ src/main/java/frc/robot/subsystems/end_effector/EndEffectorIOVortex.java
@@ -37,9 +37,9 @@ public class EndEffectorIOVortex implements EndEffectorIO {
         SparkFlexConfig algaeWristconfig = new SparkFlexConfig();
 
         algaeWristconfig.closedLoop
-        .p(4.0)
+        .p(169.0)
         .i(0.0)
-        .d(0.0)
+        .d(50)
         .outputRange(-12.0, 12.0);
 
         algaeWristconfig.closedLoop.maxMotion
--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 384;
-  public static final String GIT_SHA = "3b9951c31532f86ce3796eb9b7f6b185f0eb6886";
-  public static final String GIT_DATE = "2025-07-20 20:11:26 CDT";
+  public static final int GIT_REVISION = 385;
+  public static final String GIT_SHA = "2b519365bf6a2388395bed30c4e76661e52e7ddd";
+  public static final String GIT_DATE = "2025-07-20 21:34:19 CDT";
   public static final String GIT_BRANCH = "maaz_sim";
-  public static final String BUILD_DATE = "2025-07-20 20:44:11 CDT";
-  public static final long BUILD_UNIX_TIME = 1753062251246L;
+  public static final String BUILD_DATE = "2025-07-21 00:27:17 CDT";
+  public static final long BUILD_UNIX_TIME = 1753075637075L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
